7d0a8764fc42e1e774587357eb0f938136d1ae5a
haoxin <haoxiner@vip.qq.com>
2017-05-31 20:51:39 +0800

    Read And Write Image In Compute Shader.

--- Shiny/Shaders/PreComputation.comp.glsl
+++ Shiny/Shaders/PreComputation.comp.glsl
@@ -1,13 +1,51 @@
 #version 450 core
-layout (binding = 0, std430) buffer result
+#define PI 3.1415926535897932384626433832795
+#define INV_PI (1.0/PI)
+const int sampleCount = 1024;
+
+layout (binding = 0, std140) uniform SamplesBuffer
 {
-	int sum;
-	int sum2;
+	vec4 samples[sampleCount / 2];
 };
+
+layout (binding = 0) uniform sampler2D inputEnvmap;
+layout (rgba32f, binding = 0) uniform image2D outputEnvmap;
+
+vec4 SamplePanorama(sampler2D panorama, vec3 direction)
+{
+	vec2 uv = vec2(PI + atan(direction.x, -direction.z), acos(-direction.y)) / vec2(2.0 * PI, PI);
+	return texture(panorama, uv);
+}
 
 float saturate(float value)
 {
 	return clamp(value, 0.0, 1.0);
+}
+
+vec2 hammersley(uint originalSample)
+{
+	uint revertSample;
+	const float g_m = 10.0;
+	const float u_binaryFractionFactor = 1.0 / (sampleCount - 1.0);
+
+	// Revert bits by swapping blockwise. Lower bits are moved up and higher bits down.
+	revertSample = (originalSample << 16u) | (originalSample >> 16u);
+	revertSample = ((revertSample & 0x00ff00ffu) << 8u) | ((revertSample & 0xff00ff00u) >> 8u);
+	revertSample = ((revertSample & 0x0f0f0f0fu) << 4u) | ((revertSample & 0xf0f0f0f0u) >> 4u);
+	revertSample = ((revertSample & 0x33333333u) << 2u) | ((revertSample & 0xccccccccu) >> 2u);
+	revertSample = ((revertSample & 0x55555555u) << 1u) | ((revertSample & 0xaaaaaaaau) >> 1u);
+
+	// Shift back, as only m bits are used.
+	revertSample = revertSample >> (32 - u_m);
+
+	return vec2(float(revertSample) * u_binaryFractionFactor, float(originalSample) * u_binaryFractionFactor);
+}
+
+vec2 getSample(int i, int total)
+{
+	int index = i / 2;
+	int offset = int(mod(i, int(2)));
+	return offset == 0 ? samples[index].xy : samples[index].zw;
 }
 
 vec3 F_Schlick(in vec3 f0, in float f90, in float u)
@@ -31,7 +69,13 @@ float V_SmithGGXCorrelated(float NdotL, float NdotV, float alphaG)
 
 	return 0.5f / (Lambda_GGXV + Lambda_GGXL);
 }
-
+float G_SmithGGXCorrelated(float NdotL, float NdotV, float alphaG)
+{
+	float alphaG2 = alphaG * alphaG;
+	float Lambda_GGXV = NdotL * sqrt((-NdotV * alphaG2 + NdotV) * NdotV + alphaG2);
+	float Lambda_GGXL = NdotV * sqrt((-NdotL * alphaG2 + NdotL) * NdotL + alphaG2);
+	return 2.0 * NdotL * NdotV / (Lambda_GGXV + Lambda_GGXL);
+}
 float D_GGX(float NdotH, float m)
 {
 	// Divide by PI is apply later
@@ -46,8 +90,8 @@ float Fr_DisneyDiffuse(
 	float LdotH,
 	float linearRoughness)
 {
-	float energyBias = lerp(0, 0.5, linearRoughness);
-	float energyFactor = lerp(1.0, 1.0 / 1.51, linearRoughness);
+	float energyBias = mix(0.0, 0.5, linearRoughness);
+	float energyFactor = mix(1.0, 1.0 / 1.51, linearRoughness);
 	float fd90 = energyBias + 2.0 * LdotH*LdotH * linearRoughness;
 	vec3 f0 = vec3(1.0f, 1.0f, 1.0f);
 	float lightScatter = F_Schlick(f0, fd90, NdotL).r;
@@ -69,27 +113,66 @@ void importanceSampleCosDir(
 	float u1 = u.x;
 	float u2 = u.y;
 	float r = sqrt(u1);
-	float phi = u2 * FB_PI * 2;
+	float phi = u2 * PI * 2;
 	L = vec3(r*cos(phi), r*sin(phi), sqrt(max(0.0f,1.0f-u1)));
 	L = normalize(tangentX * L.y + tangentY * L.x + N * L.z);
 	NdotL = dot(L,N);
-	pdf = NdotL * FB_INV_PI;
+	pdf = NdotL * INV_PI;
 }
-
+struct Referential
+{
+	vec3 upVector;
+	vec3 tangentX;
+	vec3 tangentY;
+};
+Referential createReferential(vec3 N)
+{
+	Referential referential;
+	referential.upVector = abs(N.z) < 0.999 ? vec3(0,0,1) : vec3(1,0,0);
+	referential.tangentX = normalize( cross( referential.upVector, N ) );
+	referential.tangentY = cross( N, referential.tangentX );
+	return referential;
+}
+vec3 importanceSampleGGX( vec2 Xi, float Roughness, vec3 N, Referential referential )
+{
+	float a = Roughness * Roughness;
+	float Phi = 2 * PI * Xi.x;
+	float CosTheta = sqrt( (1 - Xi.y) / ( 1 + (a*a - 1) * Xi.y ) );
+	float SinTheta = sqrt( 1 - CosTheta * CosTheta );
+	vec3 H;
+	H.x = SinTheta * cos( Phi );
+	H.y = SinTheta * sin( Phi );
+	H.z = CosTheta;
+	// Tangent to world space
+	return referential.tangentX * H.x + referential.tangentY * H.y + N * H.z;
+}
+void importanceSampleGGX_G(
+	in vec2 u, in vec3 V, in vec3 N,
+	in Referential referential, in float roughness,
+	out float NdotH, out float LdotH, out vec3 L, out float G)
+{
+	vec3 H = importanceSampleGGX(u, roughness, N, referential);
+	L = 2 * dot( V, H ) * H - V;
+	NdotH = saturate(dot(N, H));
+	LdotH = saturate(dot(L, H));
+	float NdotL = saturate(dot(N, L));
+	float NdotV = saturate(dot(N, V)); 
+	G = G_SmithGGXCorrelated(NdotL, NdotV, roughness);
+}
 vec4 integrateDFGOnly(
 	in vec3 V,
 	in vec3 N,
 	in float roughness)
 {
 	float NdotV = saturate(dot(N, V));
-	vec4 acc = 0;
-	float accWeight = 0;
+	vec4 acc = vec4(0.0);
+	float accWeight = 0.0;
 	// Compute pre-integration
 	Referential referential = createReferential(N);
-	for (uint i=0; i<sampleCount; ++i)
+	for (int i=0; i<sampleCount; ++i)
 	{
 		vec2 u = getSample(i, sampleCount);
-		vec3 L = 0;
+		vec3 L = vec3(0);
 		float NdotH = 0;
 		float LdotH = 0;
 		float G = 0;
@@ -105,14 +188,14 @@ vec4 integrateDFGOnly(
 			acc.y += Fc*GVis;
 		}
 		// diffuse Disney preIntegration
-		u = frac(u + 0.5);
+		u = fract(u + 0.5);
 		float pdf;
 		// The pdf is not use because it cancel with other terms
 		// (The 1/PI from diffuse BRDF and the NdotL from Lambert ’s law).
 		importanceSampleCosDir(u, N, L, NdotL , pdf);
 		if (NdotL >0)
 		{
-			float LdotH = saturate(dot(L, normalize(V + L));
+			float LdotH = saturate(dot(L, normalize(V + L)));
 			float NdotV = saturate(dot(N, V));
 			acc.z += Fr_DisneyDiffuse(NdotV , NdotL , LdotH , sqrt(roughness));
 		}
@@ -121,9 +204,26 @@ vec4 integrateDFGOnly(
 	return acc * (1.0f / accWeight);
 }
 
-layout(local_size_x = 32, local_size_y = 32) in;
+vec4 integrateDiffuseCube(in vec3 N)
+{
+	vec3 accBrdf = vec3(0);
+	for (int i=0; i<sampleCount; ++i)
+	{
+		vec2 eta = getSample(i, sampleCount);
+		vec3 L;
+		float NdotL;
+		float pdf;
+		// see reference code in appendix
+		importanceSampleCosDir(eta, N, L, NdotL , pdf);
+		if (NdotL >0)
+			accBrdf += SamplePanorama(inputEnvmap, L).rgb;//IBLCube.Sample(incomingLightSampler , L).rgb;
+	}
+	return vec4(accBrdf * (1.0f / sampleCount), 1.0f);
+}
+
+layout(local_size_x = 1, local_size_y = 1) in;
 void main()
 {
-	atomicAdd(sum, 1);
-	atomicAdd(sum2, 2);
+	imageStore(outputEnvmap, ivec2(gl_GlobalInvocationID.xy),
+		texelFetch(inputEnvmap, ivec2(gl_GlobalInvocationID.xy), 0));
 }
--- Shiny/Shiny.cpp
+++ Shiny/Shiny.cpp
@@ -5,6 +5,7 @@
 #include "ShaderProgram.h"
 #include "Game.h"
 #include "ResourceManager.h"
+#include "FreeImage.h"
 #include <fstream>
 #include <iostream>
 
@@ -47,29 +48,54 @@ int TestComputShader()
         return 1;
     }
 
-    Shiny::ShaderProgram computeShaderProgram;
-    computeShaderProgram.Startup(Shiny::ResourceManager::ReadFileToString("./Shaders/LightCulling.comp.glsl"));
-    computeShaderProgram.Use();
-    GLuint buffer;
-    glCreateBuffers(1, &buffer);
-    int initData[2] = { 0,0 };
-    glNamedBufferStorage(buffer, sizeof(int) * 2, &initData, GL_MAP_READ_BIT | GL_MAP_WRITE_BIT);
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, buffer);
-    auto pBuffer = glMapNamedBuffer(buffer, GL_WRITE_ONLY);
-    *(int*)pBuffer = 1024;
-    *((int*)pBuffer + 1) = 0;
-    glUnmapNamedBuffer(buffer);
 
-    glDispatchCompute(1, 1, 1);
-    pBuffer = glMapNamedBuffer(buffer, GL_READ_ONLY);
-    std::cerr << *(int*)pBuffer << std::endl;
-    std::cerr << *((int*)pBuffer + 1) << std::endl;
-    glUnmapNamedBuffer(buffer);
+    GLuint textureID_;
+    FIBITMAP* dib = FreeImage_Load(FIF_EXR, "../../uffizi.exr");
+    auto colorType = FreeImage_GetColorType(dib);
+    auto bpp = FreeImage_GetBPP(dib);
+    auto w = FreeImage_GetWidth(dib);
+    auto h = FreeImage_GetHeight(dib);
+    std::cerr << w << "," << h << std::endl;
+    auto bits = FreeImage_GetBits(dib);
+    glCreateTextures(GL_TEXTURE_2D, 1, &textureID_);
+    glTextureStorage2D(textureID_, 1, GL_RGB32F, w, h);
+    glTextureSubImage2D(textureID_, 0, 0, 0, w, h, GL_RGB, GL_FLOAT, bits);
+    FreeImage_Unload(dib);
+    glBindTextureUnit(0, textureID_);
+
+    //auto w = 512, h = 512;
+    GLuint outputTextureID;
+    glCreateTextures(GL_TEXTURE_2D, 1, &outputTextureID);
+    glTextureStorage2D(outputTextureID, 1, GL_RGBA32F, w, h);
+    glBindImageTexture(0, outputTextureID, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+
+
+    Shiny::ShaderProgram computeShaderProgram;
+    computeShaderProgram.Startup(Shiny::ResourceManager::ReadFileToString("./Shaders/PreComputation.comp.glsl"));
+    computeShaderProgram.Use();
+    //GLuint buffer;
+    //glCreateBuffers(1, &buffer);
+    //int initData[2] = { 0,0 };
+    //glNamedBufferStorage(buffer, sizeof(int) * 2, &initData, GL_MAP_READ_BIT | GL_MAP_WRITE_BIT);
+    //glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, buffer);
+
+    glDispatchCompute(w, h, 1);
+    std::cerr << "output" << std::endl;
+    auto texSize = sizeof(float) * 3 * w * h;
+    auto bitmap = FreeImage_AllocateT(FIT_RGBF,w, h);
+    auto pixels = FreeImage_GetBits(bitmap);
+    //new BYTE[bufSize];
+    std::cerr << (pixels ? 1 : 0) << std::endl;
+
+    glGetTextureImage(outputTextureID, 0, GL_RGB, GL_FLOAT, texSize, pixels);
+    
+    auto result = FreeImage_Save(FIF_EXR, bitmap, "../../output.exr", EXR_DEFAULT);
+    std::cerr << result << std::endl;
     display.Shutdown();
     return 0;
 }
 int main()
 {
-    TestDisplay();
+    TestComputShader();
     return 0;
 }

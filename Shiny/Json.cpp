faf982993faad51a1447a95d4505071f0062c494
haoxin <haoxiner@vip.qq.com>
2017-06-08 12:02:02 +0800

    Can parse string member of json now.

--- Shiny/Json.cpp
+++ Shiny/Json.cpp
@@ -1,4 +1,38 @@
 #include "Json.h"
+#include <cctype>
+
+const Shiny::Json::JsonValue Shiny::Json::JsonValue::NULL_OBJECT;
+
+Shiny::Json::JsonValue::JsonValue() : type_(JSON_NULL), value_({ false })
+{
+}
+
+Shiny::Json::JsonValue::JsonValue(float number) : type_(JSON_NUMBER), value_({ number })
+{
+}
+
+Shiny::Json::JsonValue::JsonValue(bool boolean) : type_(JSON_BOOL), value_({ boolean })
+{
+}
+
+Shiny::Json::JsonValue::JsonValue(const char* str) : type_(JSON_STRING), value_({ str })
+{
+}
+
+Shiny::Json::JsonValue::JsonValue(const JsonObject* object) : type_(JSON_OBJECT), value_({ object })
+{
+}
+
+void Shiny::Json::JsonValue::Destroy()
+{
+    if (type_ == JSON_STRING) {
+        delete[] value_.str;
+    } else if (type_ == JSON_OBJECT) {
+        delete value_.object;
+    } else if (type_ >= JSON_ARRAY) {
+        delete value_.array;
+    }
+}
 
 int Shiny::Json::JsonValue::AsInt() const
 {
@@ -18,36 +52,36 @@ float Shiny::Json::JsonValue::AsFloat() const
 
 Shiny::Float2 Shiny::Json::JsonValue::AsFloat2() const
 {
-    if (type_ == JSON_ARRAY) {
-        return Float2(value_.array[0], value_.array[1]);
+    if (type_ == JSON_ARRAY + 2) {
+        return Float2(value_.array[0].AsFloat(), value_.array[1].AsFloat());
     }
     return Float2();
 }
 
 Shiny::Float3 Shiny::Json::JsonValue::AsFloat3() const
 {
-    if (type_ == JSON_ARRAY) {
-        return Float3(value_.array[0], value_.array[1], value_.array[2]);
+    if (type_ == JSON_ARRAY + 3) {
+        return Float3(value_.array[0].AsFloat(), value_.array[1].AsFloat(), value_.array[2].AsFloat());
     }
     return Float3();
 }
 
 Shiny::Float4 Shiny::Json::JsonValue::AsFloat4() const
 {
-    if (type_ == JSON_ARRAY) {
-        return Float4(value_.array[0], value_.array[1], value_.array[2], value_.array[3]);
+    if (type_ == JSON_ARRAY + 4) {
+        return Float4(value_.array[0].AsFloat(), value_.array[1].AsFloat(), value_.array[2].AsFloat(), value_.array[3].AsFloat());
     }
     return Float4();
 }
 
 Shiny::Matrix4x4 Shiny::Json::JsonValue::AsMatrix4x4() const
 {
-    if (type_ == JSON_ARRAY) {
+    if (type_ == JSON_ARRAY + 16) {
         return Matrix4x4(
-            value_.array[0], value_.array[1], value_.array[2], value_.array[3],
-            value_.array[4], value_.array[5], value_.array[6], value_.array[7],
-            value_.array[8], value_.array[9], value_.array[10], value_.array[11],
-            value_.array[12], value_.array[13], value_.array[14], value_.array[15]);
+            value_.array[0].AsFloat(), value_.array[1].AsFloat(), value_.array[2].AsFloat(), value_.array[3].AsFloat(),
+            value_.array[4].AsFloat(), value_.array[5].AsFloat(), value_.array[6].AsFloat(), value_.array[7].AsFloat(),
+            value_.array[8].AsFloat(), value_.array[9].AsFloat(), value_.array[10].AsFloat(), value_.array[11].AsFloat(),
+            value_.array[12].AsFloat(), value_.array[13].AsFloat(), value_.array[14].AsFloat(), value_.array[15].AsFloat());
     }
     return Matrix4x4();
 }
@@ -71,17 +105,15 @@ std::string Shiny::Json::JsonValue::AsString() const
         return std::string("JSON_OBJECT");
         break;
     case Shiny::Json::JSON_ARRAY:
-        return std::string("JSON_ARRAY");
-        break;
     default:
-        break;
+        return std::string("JSON_ARRAY SIZE = " + std::to_string(type_));
     }
     return std::string();
 }
 
-const Shiny::Json::JsonArray* Shiny::Json::JsonValue::AsJsonArray() const
+const Shiny::Json::JsonValue* Shiny::Json::JsonValue::AsJsonArray() const
 {
-    if (type_ == JSON_ARRAY) {
+    if (type_ >= JSON_ARRAY) {
         return value_.array;
     }
     return nullptr;
@@ -95,57 +127,179 @@ const Shiny::Json::JsonObject* Shiny::Json::JsonValue::AsJsonObject() const
     return nullptr;
 }
 
-Shiny::Json::JsonValue* Shiny::Json::JsonObject::GetValue(std::string& key)
+Shiny::Json::JsonObject::~JsonObject()
+{
+    for (auto&& keyValue : valueTable_) {
+        keyValue.second.Destroy();
+    }
+}
+
+Shiny::Json::JsonValue& Shiny::Json::JsonObject::GetValue(const std::string& key)
 {
     auto valueIter = valueTable_.find(key);
     if (valueIter != valueTable_.end()) {
         return valueIter->second;
     }
-    return nullptr;
+    return JsonValue();
 }
 
-Shiny::Json::JsonValue* Shiny::Json::JsonArray::operator[](int i)
+bool Shiny::Json::Parse(JsonObject& jsonObject, const char* json, const size_t length)
 {
-    return array_[i];
+    return false;
 }
 
-class ParserState
+Shiny::Json::Parser::Parser(JsonObject* jsonObject, const char* json, const size_t length)
+    : json_(json), length_(length), jsonObject_(jsonObject)
 {
-public:
-    ParserState(const std::string& json): length(json.length())
-    {
+    ch_ = json_[0];
+    SkipSpacesAndComments();
+    if (ch_ != '{') {
+        hasError_ = true;
     }
-    void Forward()
-    {
-        offset++;
-    }
+    ParseObject(jsonObject);
+}
 
-    
-};
-
-bool Shiny::Json::Parse(JsonObject& jsonObject, const std::string& json)
+bool Shiny::Json::Parser::HasError()
 {
-    size_t offset = 0;
-    const size_t length = json.length();
-    enum ParserState
-    {
-        READY,
-        OBJECT_BEGIN,
-        STOP
-    };
-    ParserState state = READY;
-    if (offset < length) {
+    return hasError_;
+}
 
+bool Shiny::Json::Parser::Forward()
+{
+    if (finish_ || hasError_) {
+        return false;
     }
-    while (offset < length && state != STOP) {
-        char ch = json[offset];
-        switch (ch) {
-        case '{':
-            state = OBJECT_BEGIN;
-            break;
-        default:
-            break;
+    if (ch_ == '\n') {
+        column_ = 1;
+        row_++;
+    } else {
+        column_++;
+    }
+    offset_++;
+    if (offset_ >= length_) {
+        return false;
+    }
+    ch_ = json_[offset_];
+    if (ch_ == 0) {
+        hasError_ = true;
+        return false;
+    }
+    return true;
+}
+
+void Shiny::Json::Parser::Forward(size_t count)
+{
+    for (size_t i = 0; i < count; i++) {
+        Forward();
+    }
+}
+
+bool Shiny::Json::Parser::SkipSpaces()
+{
+    bool found = false;
+    if (std::isspace(ch_)) {
+        found = true;
+        while (Forward() && std::isspace(ch_)) {}
+    }
+    return found;
+}
+
+bool Shiny::Json::Parser::SkipComments()
+{
+    bool found = false;
+    if (ch_ == '/') {
+        if (Forward() && ch_ == '/') {
+            found = true;
+            int nextRow = row_ + 1;
+            while (Forward() && row_ < nextRow) {}
+        }
+    }
+    return found;
+}
+
+void Shiny::Json::Parser::SkipSpacesAndComments()
+{
+    while (SkipSpaces() || SkipComments()) {
+        // break until there are no spaces and comments
+    }
+}
+
+bool Shiny::Json::Parser::ParseObject(JsonObject* object)
+{
+    while (Forward() && ch_ != '}') {
+        SkipSpacesAndComments();
+        if (ch_ == '"') {
+            auto key = ParseString();
+            if (hasError_) {
+                break;
+            }
+            if (jsonObject_->valueTable_.find(key) != jsonObject_->valueTable_.end()) {
+                hasError_ = true;
+                break;
+            }
+            SkipSpacesAndComments();
+            if (ch_ == ':') {
+                Forward();
+                SkipSpacesAndComments();
+                auto str = ParseString();
+                if (hasError_) {
+                    break;
+                } else {
+                    char* value = new char[str.length() + 1];
+                    str.copy(value, str.length());
+                    value[str.length()] = '\0';
+                    jsonObject_->valueTable_.emplace(std::make_pair(key, JsonValue(value)));
+                }
+            }
         }
     }
     return false;
+}
+
+std::pair<size_t, size_t> Shiny::Json::Parser::FindStringRange()
+{
+    SkipSpacesAndComments();
+    if (ch_ != '"') {
+        hasError_ = true;
+        return std::pair<size_t, size_t>(0, 0);
+    }
+    auto begin = offset_;
+    auto end = begin;
+    while (end < length_ && json_[end] != '"') {
+        end++;
+    }
+    if (end < length_ && json_[end] == '"') {
+        end++;
+        return std::pair<size_t, size_t>(begin, end);
+    }
+    hasError_ = true;
+    return std::pair<size_t, size_t>(0, 0);
+}
+
+std::string Shiny::Json::Parser::ParseString()
+{
+    if (ch_ != '"') {
+        hasError_ = true;
+        return "";
+    }
+    auto begin = offset_;
+    auto end = begin + 1;
+    while (end < length_ && json_[end] != '"') {
+        end++;
+    }
+    if (end < length_ && json_[end] == '"') {
+        end++;
+        Forward(end - begin);
+        if (begin + 2 == end) {
+            return "";
+        }
+        return std::string(json_ + begin + 1, json_ + end - 1);
+    }
+    hasError_ = true;
+    return "";
+}
+
+float Shiny::Json::Parser::ParseNumber()
+{
+    return 0.0f;
 }
--- Shiny/Json.h
+++ Shiny/Json.h
@@ -3,24 +3,30 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <utility>
 namespace Shiny
 {
 namespace Json
 {
 enum ValueType
 {
-    JSON_NULL,
-    JSON_BOOL,
-    JSON_NUMBER,
-    JSON_STRING,
-    JSON_OBJECT,
-    JSON_ARRAY
+    JSON_NULL = -5,
+    JSON_BOOL = -4,
+    JSON_NUMBER = -3,
+    JSON_STRING = -2,
+    JSON_OBJECT = -1,
+    JSON_ARRAY = 0
 };
 class JsonObject;
-class JsonArray;
 class JsonValue
 {
 public:
+    JsonValue();
+    JsonValue(float number);
+    JsonValue(bool boolean);
+    JsonValue(const char* str);
+    JsonValue(const JsonObject* object);
+    void Destroy();
     int AsInt() const;
     float AsFloat() const;
     Float2 AsFloat2() const;
@@ -28,34 +34,70 @@ public:
     Float4 AsFloat4() const;
     Matrix4x4 AsMatrix4x4() const;
     std::string AsString() const;
-    const JsonArray* AsJsonArray() const;
+    const JsonValue* AsJsonArray() const;
     const JsonObject* AsJsonObject() const;
 private:
     union Value
     {
-        bool boolean;
-        float number;
-        char* str;
-        JsonArray* array;
-        JsonObject* object;
+        Value(bool value) : boolean(value) {}
+        Value(float value) : number(value) {}
+        Value(const char* value) : str(value) {}
+        Value(const JsonObject* value) : object(value) {}
+        Value(const JsonValue* value) : array(value) {}
+        const bool boolean;
+        const float number;
+        const char* str;
+        const JsonObject* object;
+        const JsonValue* array;
     };
-    Value value_;
-    ValueType type_;
-    std::string name_;
+    const Value value_ = { false };
+    const ValueType type_ = JSON_NULL;
+    static const JsonValue NULL_OBJECT;
 };
-class JsonObject : public JsonValue
-{
-    JsonValue* GetValue(std::string& key);
-private:
-    std::map<std::string, JsonValue*> valueTable_;
-};
-class JsonArray : public JsonValue
+class Parser
 {
 public:
-    JsonValue* operator[](int i);
+    Parser(JsonObject* jsonObject, const char* json, const size_t length);
+    bool HasError();
 private:
-    std::vector<JsonValue*> array_;
+    bool Forward();
+    void Forward(size_t count);
+    bool SkipSpaces();
+    bool SkipComments();
+    void SkipSpacesAndComments();
+
+    bool ParseObject(JsonObject* object);
+
+    std::pair<size_t, size_t> FindStringRange();
+    std::string ParseString();
+    float ParseNumber();
+
+    const char* json_ = nullptr;
+    const size_t length_ = 0;
+    size_t offset_ = 0;
+    JsonObject* jsonObject_;
+    char ch_ = 0;
+    bool hasError_ = false;
+    bool finish_ = false;
+    size_t row_ = 1;
+    size_t column_ = 1;
 };
-bool Parse(JsonObject& jsonObject, const std::string& json);
+class JsonObject
+{
+    friend class Parser;
+public:
+    ~JsonObject();
+    JsonValue& GetValue(const std::string& key);
+private:
+    std::map<std::string, JsonValue> valueTable_;
+};
+//class JsonArray
+//{
+//public:
+//    JsonValue& operator[](int i);
+//private:
+//    std::vector<JsonValue> array_;
+//};
+bool Parse(JsonObject& jsonObject, const char* json, const size_t length);
 }
 }
